eba8099a6a1cc9f80b3f2b5c8822121e73672957
manu-panachiyil <manupanachiyil999@gmail.com>
2024-01-30 16:29:11 +0530

    delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -149,6 +149,24 @@ function Profile() {
     }
   }
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE'
+      })
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile </h1>
@@ -234,7 +252,7 @@ function Profile() {
           <p>{listing.name} </p>
         </Link>
         <div className="flex flex-col items-center">
-          <button className="text-red-700 uppercase">delete</button>
+          <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">delete</button>
           <button className="text-green-700 uppercase">edit</button>
         </div>
       </div>
